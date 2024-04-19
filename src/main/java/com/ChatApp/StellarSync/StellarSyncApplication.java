11fda187e568984e52ad615ca0e0eedc243c43b1
ShaznaSalman <shaznasalman10@gmail.com>
2024-04-20 00:43:31 +0530

    first commit

---
+++ src/main/java/com/ChatApp/StellarSync/StellarSyncApplication.java
@@ -0,0 +1,13 @@
+package com.ChatApp.StellarSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChatApp/StellarSync/StellarSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChatApp.StellarSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
